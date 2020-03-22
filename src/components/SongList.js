bc4a1112a755223e9f84f01678412e9b32128b67
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-03-22 22:40:13 +0900

    Enable action on SongList component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends React.Component {
     renderList() {
@@ -7,7 +8,10 @@ class SongList extends React.Component {
             return (
                 <div className="item" key={song.title}>
                     <div className="right floated content">
-                        <button className=" ui button primary">Select</button>
+                        <button
+                            className=" ui button primary"
+                            onClick={() => this.props.selectSong(song)}
+                        >Select</button>
                     </div>
                     <div className="content">{song.title}</div>
                 </div>
@@ -15,6 +19,7 @@ class SongList extends React.Component {
         })
     }
     render() {
+        console.log(this.props);
         return <div className="ui devided list">{this.renderList()}</div>;
     }
 }
@@ -23,4 +28,4 @@ const mapStateToProps = (state) => {
     return { songs: state.songs }
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, { selectSong })(SongList);
